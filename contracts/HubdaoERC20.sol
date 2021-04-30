7b4793154cbbd7bcd175ab530d95c62ea18e8e21
harold <harold@funit.io>
2021-04-30 17:12:06 +0900

    LP Token Symbol

--- contracts/HubdaoERC20.sol
+++ contracts/HubdaoERC20.sol
@@ -7,7 +7,7 @@ contract HubdaoERC20 is IHubdaoERC20 {
     using SafeMath for uint;
 
     string public constant name = 'Hubdao LPs';
-    string public constant symbol = 'Cake-LP';
+    string public constant symbol = 'Hub-LP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
